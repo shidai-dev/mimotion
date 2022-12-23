ea5d9162062009949ef204bb6987c0af176ca6af
shidai-dev <88641603+shidai-dev@users.noreply.github.com>
2022-12-23 11:38:15 +0800

    Update main.py
    支持邮箱和手机号登陆

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ import time
 from urllib.parse import quote
 
 import requests
+email=re.compile(r'[a-z0-9\-\.]+@[0-9a-z\-\.]+')
 
 now = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 headers = {
@@ -33,7 +34,16 @@ def login(_user, password):
     """
     登录
     """
-    url1 = "https://api-user.huami.com/registrations/+86" + _user + "/tokens"
+    login_type = "huami_phone"
+    index1=_user.find("@")
+    if index1 != -1:
+        print("邮箱")
+        login_type = "email"
+        url1 = "https://api-user.huami.com/registrations/"+_user+"/tokens"
+    else:
+        url1 = "https://api-user.huami.com/registrations/+86" + _user + "/tokens"
+        print("手机")
+    
     _headers = {
         "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
         "User-Agent": "MiFit/4.6.0 (iPhone; iOS 14.0.1; Scale/2.00)"
@@ -50,27 +60,32 @@ def login(_user, password):
         code = get_code(location)
     except:
         return 0, 0
-    # print("access_code获取成功！")
-    # print(code)
+    #print("access_code获取成功！")
+    #print(code)
 
     url2 = "https://account.huami.com/v2/client/login"
     data2 = {
+        "allow_registration":"false",
         "app_name": "com.xiaomi.hm.health",
-        "app_version": "4.6.0",
+        "app_version": "6.3.5",
         "code": f"{code}",
         "country_code": "CN",
         "device_id": "2C8B4939-0CCD-4E94-8CBA-CB8EA6E613A1",
         "device_model": "phone",
         "grant_type": "access_token",
-        "third_name": "huami_phone",
+        "third_name": f"{login_type}",
+        "lang":"zh_CN",
+        "os_version":"1.5.0",
+        "source":"com.xiaomi.hm.health",
     }
     r2 = requests.post(url2, data=data2, headers=_headers).json()
+    #print(r2)
     login_token = r2["token_info"]["login_token"]
-    # print("login_token获取成功！")
-    # print(login_token)
+    #print("login_token获取成功！")
+    #print(login_token)
     userid = r2["token_info"]["user_id"]
-    # print("userid获取成功！")
-    # print(userid)
+    #print("userid获取成功！")
+    #print(userid)
 
     return login_token, userid
 
